ef49100c98c8e8cb9cad4df3ccfd69a50c895b90
Raksfai <oleksander.veresaka05@gmail.com>
2025-08-14 23:46:09 +0300

    add function declaration for get_files_info

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from functions.get_files_info import schema_get_files_info
 from system_prompt import system_prompt
 
 def main():
@@ -18,6 +19,14 @@ def main():
         print("Error 1")
         sys.exit(1)
 
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
+    
+    
+
     messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
 
     # GenerateContentResponse = client.models.generate_content(model="gemini-2.0-flash", contents=messages)
@@ -25,14 +34,23 @@ def main():
     GenerateContentResponse = client.models.generate_content(
         model="gemini-2.0-flash",
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt),
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
     )
-
+    
     if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
-        print(GenerateContentResponse.text)
+        check_function_calls(GenerateContentResponse)
         print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {GenerateContentResponse.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {GenerateContentResponse.usage_metadata.candidates_token_count}")
+    else:
+        check_function_calls(GenerateContentResponse)
+
+def check_function_calls(GenerateContentResponse):
+    if GenerateContentResponse.function_calls is not None:
+        for call in GenerateContentResponse.function_calls:
+            print(f"Calling function: {call.name}({call.args})")   
     else:
         print(GenerateContentResponse.text)
 

e3b49701a45129ae89ab36fa6475e8a5b632d097
Raksfai <oleksander.veresaka05@gmail.com>
2025-08-15 12:58:39 +0300

    add three schemas for avaible func for ai_agent

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,5 +1,6 @@
 import os
 from config import MAX_CHARS
+from google.genai import types
 
 def get_file_content(working_directory, file_path):
     abs_path = os.path.abspath(working_directory)
@@ -22,3 +23,21 @@ def get_file_content(working_directory, file_path):
     except Exception as e:
         return f"Error: {e}"
 
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description=f"Reads and returns the first {MAX_CHARS} characters of the content from a specified file within the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "working_directory": types.Schema(
+                type=types.Type.STRING,
+                description="The working directory path (usually '.')",
+            ),
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file whose content should be read, relative to the working directory.",
+            ),
+        },
+        required=["working_directory", "file_path"],
+    ),
+)
--- main.py
+++ main.py
@@ -4,8 +4,11 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-from functions.get_files_info import schema_get_files_info
 from system_prompt import system_prompt
+from functions.get_files_info import schema_get_files_info
+from functions.get_file_content import schema_get_file_content
+from functions.run_python import schema_run_python_file
+from functions.write_file import schema_write_file
 
 def main():
     load_dotenv()
@@ -22,6 +25,9 @@ def main():
     available_functions = types.Tool(
         function_declarations=[
             schema_get_files_info,
+            schema_get_file_content,
+            schema_run_python_file,
+            schema_write_file
         ]
     )
     
